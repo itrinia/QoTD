fce70341bb8d7aca48932c60ab5e5a13eff7c89a
itrinia <94580529+itrinia@users.noreply.github.com>
2023-05-06 14:58:47 +0700

    Initial Commit

---
+++ QoTD/QoTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  QoTDApp.swift
+//  QoTD
+//
+//  Created by MacBook Pro on 06/05/23.
+//
+
+import SwiftUI
+
+@main
+struct QoTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
